6b5cb5b1edd0c682d474f4eb515e507c669aef11
MartinFrank77 <martin.frank@irix.ch>
2025-12-04 17:30:02 +0100

    added metrics analyse

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 import AdventCalendar from './components/AdventCalendar';
+import { clarity } from 'react-microsoft-clarity';
 
 function App() {
+  clarity.init('uem2s1oder');
   return (
     <div className="App">
         <h1>Advent Calendar 2025</h1>
